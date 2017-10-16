c5a7e5c774d613afd205b637b2a2f16c460bdf28
Milliam Maxime Zekeng Ndadji <Milliam Maxime Zekeng Ndadji>
2017-10-16 12:24:57 +0100

    A stable version for this morning after merger today.

---
+++ Research Outcome Publishing/WebContent/modules/payment/payment_status.jsp
@@ -0,0 +1,2 @@
+<h1><c:out value="${idiot}"></c:out></h1>
+<h1><c:out value="${statusDesc}"></c:out></h1>
--- Research Outcome Publishing/src/rop/miu/modules/payment/ModPayment.java
+++ Research Outcome Publishing/src/rop/miu/modules/payment/ModPayment.java
@@ -1,18 +1,18 @@
 package rop.miu.modules.payment;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import rop.miu.beans.BaoAccessCoupon;
-import rop.miu.beans.BaoUser;
 import rop.miu.modules.ServletModel;
-import rop.miu.modules.payment.dao.ROPPaymentDao;
 import rop.miu.util.IncludeManager;
-import rop.miu.util.ROPEncryptor;
-import rop.miu.util.exceptions.ROPCryptographyException;
-import rop.miu.util.exceptions.ROPDaoException;
 
 public class ModPayment extends ServletModel {
 	private static final long serialVersionUID = 1L;
@@ -23,7 +23,7 @@ public class ModPayment extends ServletModel {
        
     }
 
-	
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		super.doGet(request, response);
 		IncludeManager inclManager = getIncludeManager(request);
@@ -36,6 +36,7 @@ public class ModPayment extends ServletModel {
 			forward500(request, response);
 		}
 		if(option == null || option.equals("do-pay") || request.getSession().getAttribute("payment_access_coupon") != null){
+			request.getSession().removeAttribute("payment_access_coupon");
 			if(isConnected(request))
 				setIncludesForPayment(request);
 			else
@@ -45,15 +46,21 @@ public class ModPayment extends ServletModel {
 		}
 	}
 
-	
 	private void setIncludesForPayment(HttpServletRequest request) throws ServletException, IOException {
 		IncludeManager inclManager = getIncludeManager(request);
     	inclManager.setTitle(request, languageManager.getLanguageValue("payment_title", getLangTag(request)));
 		inclManager.addJSP(request, "/modules/payment/payment.jsp");
 		inclManager.addCSS(request, "/modules/payment/css/style.css");
 	}
+	
+	private void setIncludesForPaymentStatus(HttpServletRequest request) throws ServletException, IOException {
+		IncludeManager inclManager = getIncludeManager(request);
+    	inclManager.setTitle(request, languageManager.getLanguageValue("payment_status_title", getLangTag(request)));
+		inclManager.addJSP(request, "/modules/payment/payment_status.jsp");
+		inclManager.addCSS(request, "/modules/payment/css/style.css");
+	}
 
-
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		super.doPost(request, response);
 		IncludeManager inclManager = getIncludeManager(request);
@@ -66,12 +73,61 @@ public class ModPayment extends ServletModel {
 			forward500(request, response);
 		}
 		
-		if(option == null || option.equals("do-pay") || request.getAttribute("payment_access_coupon") != null){
+		if(option == null || option.equals("do-pay") || request.getSession().getAttribute("payment_access_coupon") != null){
+			request.getSession().removeAttribute("payment_access_coupon");
 			if(isConnected(request))
 				setIncludesForPayment(request);
 			else
 				requestAuthentication(request, response, "payment");
 			returnRequest(request, response);
+			return;
+		}
+		
+		if(option != null && option.equals("pay_with_mtn")){
+			int idbouton = new Integer(request.getParameter("idbouton"));
+			String typebouton = request.getParameter("typebouton");
+			int _amount = new Integer(request.getParameter("_amount"));
+			int _tel = new Integer(request.getParameter("telNumber"));
+			String _email=request.getParameter("_email");
+
+			request.setAttribute("idbouton", idbouton);
+			request.setAttribute("typebouton", typebouton);
+			request.setAttribute("_amount", _amount);
+			request.setAttribute("_tel", _tel);
+			request.setAttribute("_email", _email);
+
+			String var="https://developer.mtn.cm/OnlineMomoWeb/faces/transaction/transactionRequest.xhtml?idbouton="+idbouton+"&typebouton="+typebouton+"&_amount="+_amount+"&_tel="+_tel+"&_clP=&_email="+_email;
+			URL url = new URL(var);
+			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setDoOutput(true);
+			DataOutputStream paramsWriter = new DataOutputStream(conn.getOutputStream());
+			paramsWriter.flush();
+			paramsWriter.close();
+			InputStream remoteResponse = conn.getInputStream();
+			OutputStream localResponder = new OutputStream() {
+				private StringBuilder string = new StringBuilder();
+				
+				@Override
+				public void write(int x) throws IOException {
+					this.string.append((char) x );
+				}
+
+				public String toString(){
+					return this.string.toString();
+				}
+			};//.getOutputStream.getOutputStream();
+			int c;
+			while((c = remoteResponse.read()) != -1){
+				localResponder.write(c);
+			}	
+			//			localResponder.close();
+			//			remoteResponse.close();
+			conn.disconnect();
+			request.setAttribute("statusDesc", localResponder.toString());
+			request.setAttribute("idiot", "idiot");
+			setIncludesForPaymentStatus(request);
+			this.returnRequest(request, response);
 			return;
 		}
 	}
